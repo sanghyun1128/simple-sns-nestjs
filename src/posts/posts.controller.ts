c873df799705515267bd7bb688b119576a254f8c
sanghyun1128 <sanghyun1128@gmail.com>
2024-01-11 16:47:34 +0900

    Refactor: PostsController and PostsService

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  // 1) GET /posts
+  // - 모든 게시물을 가져옵니다.
+  @Get()
+  getPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  //2) GET /posts/:id
+  // - id에 해당되는 특정 게시물을 가져옵니다.
+  @Get('/:id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  //3) POST /posts
+  // - 새로운 게시물을 생성합니다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  //4) PUT /posts/:id
+  // - id에 해당되는 특정 게시물을 수정합니다.
+  @Put('/:id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(id, author, title, content);
+  }
+
+  //5) DELETE /posts/:id
+  // - id에 해당되는 특정 게시물을 삭제합니다.
+  @Delete('/:id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(id);
+  }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '2023 Yearbook Teaser',
+    content: 'happyhpappthpahphaphap',
+    likeCount: 10000000,
+    commentCount: 92232,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '2023 mmamama',
+    content: 'sdfdfsfdfdfdsfdsf',
+    likeCount: 92892382,
+    commentCount: 11132,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '2023 blue red green',
+    content: 'asldfkjsldfa;jfdkfdf',
+    likeCount: 2334985792,
+    commentCount: 98442,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts(): PostModel[] {
+    return posts;
+  }
+
+  getPostById(id: number): PostModel {
+    const post = posts.find((post) => post.id === +id);
+    if (post) {
+      return post;
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  createPost(author: string, title: string, content: string): PostModel {
+    const newPost: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts = [...posts, newPost];
+
+    return newPost;
+  }
+
+  updatePost(author: string, title: string, content: string, id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
+
+  deletePost(id: string) {
+    posts = posts.filter((post) => post.id !== +id);
+
+    if (!posts) {
+      throw new NotFoundException();
+    }
+
+    return { id: +id };
+  }
+}
