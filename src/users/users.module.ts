5bef161244e20c104c8ac49694489e977ecee709
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-08 10:54:16 +0900

    Feat: Add JwtModule and UsersModule imports to AuthModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersModel])],
+  exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService],
 })
