9cade55368ba6c86db33fe3b9af0ed1ca5c40c94
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-08 10:52:46 +0900

    Feat: Add email login and email registration endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login/email')
+  loginEmail(@Body('email') email: string, @Body('password') password: string) {
+    return this.authService.loginWithEmail({ email, password });
+  }
+
+  @Post('register/email')
+  registerEmail(
+    @Body('nickname') nickname: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.registerWithEmail({ nickname, email, password });
+  }
 }
