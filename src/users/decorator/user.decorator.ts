eaa4d2e670741c40ce684cfca1cb31880d89cb9e
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-14 14:57:53 +0900

    Feat: Add user decorator for accessing user information in requests

---
+++ src/users/decorator/user.decorator.ts
@@ -0,0 +1,19 @@
+import {
+  ExecutionContext,
+  InternalServerErrorException,
+  createParamDecorator,
+} from '@nestjs/common';
+
+export const User = createParamDecorator((data, context: ExecutionContext) => {
+  const req = context.switchToHttp().getRequest();
+
+  const user = req.user;
+
+  if (!user) {
+    throw new InternalServerErrorException(
+      'Request에 user 프로퍼티가 없습니다.',
+    );
+  }
+
+  return user;
+});
