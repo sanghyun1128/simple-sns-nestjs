1fbd2d5afe4c16b34892d0759f9c40fbb2d131ca
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-14 14:58:40 +0900

    Feat: Handle token verification errors in AuthService.verifyToken()

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -96,9 +96,13 @@ export class AuthService {
   }
 
   verifyToken(token: string) {
-    return this.jwtService.verify(token, {
-      secret: JWT_SECRET,
-    });
+    try {
+      return this.jwtService.verify(token, {
+        secret: JWT_SECRET,
+      });
+    } catch (e) {
+      throw new UnauthorizedException('토큰이 만료되었거나 잘못된 토큰입니다.');
+    }
   }
 
   rotateToken(token: string, isRefresh: boolean) {
