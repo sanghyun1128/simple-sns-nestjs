22015449f221b33944e3dc164003d95449087488
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-14 14:57:39 +0900

    Feat: Add access token guard and user decorator to postPosts method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,8 +7,12 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { User } from 'src/users/decorator/user.decorator';
 
 @Controller('posts')
 export class PostsController {
@@ -31,12 +35,13 @@ export class PostsController {
   //3) POST /posts
   // - 새로운 게시물을 생성합니다.
   @Post()
+  @UseGuards(AccessTokenGuard)
   postPosts(
-    @Body('authorId') authorId: number,
+    @User() user: UsersModel,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    return this.postsService.createPost(authorId, title, content);
+    return this.postsService.createPost(user.id, title, content);
   }
 
   //4) PUT /posts/:id
