4e309e74de5c03f3bca8fe38753811ef5b9db2c5
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-14 12:26:01 +0900

    Refactor: password.pipe.ts

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -1,13 +1,8 @@
-import {
-  PipeTransform,
-  Injectable,
-  ArgumentMetadata,
-  BadRequestException,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class PasswordPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     if (value.toString().length > 8) {
       throw new BadRequestException('비밀번호는 8자 이하이어야 합니다.');
     }
@@ -23,7 +18,7 @@ export class MaxLengthPipe implements PipeTransform {
     private readonly subject: string,
   ) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     if (value.toString().length > this.length) {
       throw new BadRequestException(
         `${this.subject}의 최대 길이는 ${this.length}자 입니다.`,
@@ -41,7 +36,7 @@ export class MinLengthPipe implements PipeTransform {
     private readonly subject: string,
   ) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     if (value.toString().length < this.length) {
       throw new BadRequestException(
         `${this.subject}의 최소 길이는 ${this.length}자 입니다.`,
