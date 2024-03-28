d7b0c29824ba1bba8229779c18b794beaa0f3b12
sanghyun1128 <sanghyun1128@gmail.com>
2024-03-28 17:19:04 +0900

    Feat: Add validation decorators to UsersModel

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -2,6 +2,10 @@ import { Column, Entity, OneToMany } from 'typeorm';
 import { RolesEnum } from '../const/roles.const';
 import { PostsModel } from 'src/posts/entities/posts.entity';
 import { BaseModel } from 'src/commmon/entity/base.entity';
+import { IsEmail, IsString, Length } from 'class-validator';
+import { lengthValidationMessage } from 'src/commmon/validation-message/length-validation.message';
+import { stringValidationMessage } from 'src/commmon/validation-message/string-validation.message';
+import { emailValidationMessage } from 'src/commmon/validation-message/email-validation.message';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -9,14 +13,32 @@ export class UsersModel extends BaseModel {
     length: 20,
     unique: true,
   })
+  @IsString({
+    message: stringValidationMessage,
+  })
+  @Length(1, 20, {
+    message: lengthValidationMessage,
+  })
   nickname: string;
 
   @Column({
     unique: true,
   })
+  @IsString({
+    message: stringValidationMessage,
+  })
+  @IsEmail(null, {
+    message: emailValidationMessage,
+  })
   email: string;
 
   @Column()
+  @IsString({
+    message: stringValidationMessage,
+  })
+  @Length(3, 8, {
+    message: lengthValidationMessage,
+  })
   password: string;
 
   @Column({
