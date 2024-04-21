f039b76577e53ff48bde46e3c55a8115fa477e19
sanghyun1128 <sanghyun1128@gmail.com>
2024-04-21 16:24:09 +0900

    Refactor: Add PaginatePostDto for pagination functionality

---
+++ src/posts/dto/paginate-post.dto.ts
@@ -0,0 +1,19 @@
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
+
+export class PaginatePostDto {
+  @IsNumber()
+  @IsOptional()
+  where__id_more_than?: number;
+
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?: number;
+
+  @IsIn(['ASC', 'DESC'])
+  @IsOptional()
+  order__createdAt: 'ASC' | 'DESC' = 'ASC';
+
+  @IsNumber()
+  @IsOptional()
+  take: number = 20;
+}
