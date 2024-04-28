a62c777186290ae46117b99789d3a3b27d048774
sanghyun1128 <sanghyun1128@gmail.com>
2024-04-28 14:52:34 +0900

    Refactor: Update PostsModule imports and providers in posts.module.ts

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,20 +1,49 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entity';
-import { AuthService } from 'src/auth/auth.service';
-import { UsersService } from 'src/users/users.service';
-import { UsersModel } from 'src/users/entities/users.entity';
 import { JwtModule } from '@nestjs/jwt';
-import { CommmonService } from 'src/commmon/commmon.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { CommmonModule } from 'src/commmon/commmon.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { POST_IMAGE_PATH } from 'src/commmon/const/path.const';
+import { v4 as uuid } from 'uuid';
 
 @Module({
   imports: [
     JwtModule.register({}),
-    TypeOrmModule.forFeature([PostsModel, UsersModel]),
+    TypeOrmModule.forFeature([PostsModel]),
+    AuthModule,
+    UsersModule,
+    CommmonModule,
+    MulterModule.register({
+      limits: {
+        fileSize: 10000000,
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+
+        if (ext !== '.jpg' && ext !== 'jpeg' && ext !== '.png') {
+          return cb(new BadRequestException('Only images are allowed'), false);
+        }
+
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, POST_IMAGE_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
   ],
   controllers: [PostsController],
-  providers: [PostsService, AuthService, UsersService, CommmonService],
+  providers: [PostsService],
 })
 export class PostsModule {}
