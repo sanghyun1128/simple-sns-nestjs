c3b7c988a4e4aeb1efe1692cf75ab9988bfa11e4
sanghyun1128 <sanghyun1128@gmail.com>
2024-04-29 10:39:15 +0900

    Feat: Add ServeStaticModule to app.module.ts for serving static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,8 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY,
 } from './commmon/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './commmon/const/path.const';
 
 @Module({
   imports: [
@@ -33,6 +35,10 @@ import {
       database: process.env[ENV_DB_DATABASE_KEY],
       entities: [PostsModel, UsersModel],
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
     }),
     PostsModule,
     UsersModule,
