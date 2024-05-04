631496c9b5629701a9aa59a4e0fbf071fe494ebd
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-04 15:09:08 +0900

    Feat: Add image upload functionality to CommonController

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,41 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { TEMP_FOLDER_PATH } from 'src/common/const/path.const';
+import { v4 as uuid } from 'uuid';
+import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 10000000,
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+
+        if (ext !== '.jpg' && ext !== 'jpeg' && ext !== '.png') {
+          return cb(new BadRequestException('Only images are allowed'), false);
+        }
+
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
