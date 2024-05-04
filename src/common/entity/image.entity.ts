e2079d6988c4efe20679f972bc7026d1de13fbc2
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-04 15:09:27 +0900

    Feat: Add ImageModel entity for managing images

---
+++ src/common/entity/image.entity.ts
@@ -0,0 +1,41 @@
+import { IsEnum, IsInt, IsOptional, IsString } from 'class-validator';
+import { BaseModel } from './base.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { POST_IMAGE_PATH } from '../const/path.const';
+import { join } from 'path';
+import { Transform } from 'class-transformer';
+import { PostsModel } from 'src/posts/entities/posts.entity';
+
+export enum ImageModelType {
+  POST_IMAGE,
+}
+
+@Entity()
+export class ImageModel extends BaseModel {
+  @Column({
+    default: 0,
+  })
+  @IsInt()
+  @IsOptional()
+  order: number;
+
+  @Column({
+    enum: ImageModelType,
+  })
+  @IsEnum(ImageModelType)
+  @IsString()
+  type: ImageModelType;
+
+  @Column()
+  @IsString()
+  @Transform(({ value, obj }) => {
+    if (obj.type === ImageModelType.POST_IMAGE) {
+      return join(POST_IMAGE_PATH, value);
+    }
+    return value;
+  })
+  path: string;
+
+  @ManyToOne((type) => PostsModel, (post) => post.images)
+  post?: PostsModel;
+}
