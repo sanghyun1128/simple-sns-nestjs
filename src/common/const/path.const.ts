0b23bf23c668d8fee1f42184fb37dd21e1c5621b
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-04 15:09:18 +0900

    Feat: Add TEMP_FOLDER_NAME constant for temporary files

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -3,6 +3,7 @@ import { join } from 'path';
 export const PROJECT_ROOT_PATH = process.cwd();
 export const PUBLIC_FOLDER_NAME = 'public';
 export const POST_FOLDER_NAME = 'posts';
+export const TEMP_FOLDER_NAME = 'temp';
 
 export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
 
@@ -12,3 +13,5 @@ export const POST_PUBLIC_IMAGE_PATH = join(
   PUBLIC_FOLDER_NAME,
   POST_FOLDER_NAME,
 );
+
+export const TEMP_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, TEMP_FOLDER_NAME);
