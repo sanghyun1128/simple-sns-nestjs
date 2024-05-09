6e166f09aa4834cea49bf66fc808cf08fb19a4a7
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-09 16:48:54 +0900

    Refactor: Improve getPostById method in PostsService

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -52,8 +52,10 @@ export class PostsService {
     );
   }
 
-  async getPostById(id: number) {
-    const post = await this.postsRepository.findOne({
+  async getPostById(id: number, qr?: QueryRunner) {
+    const repository = this.getRepository(qr);
+
+    const post = await repository.findOne({
       ...DEFAULT_POST_FIND_OPTIONS,
       where: {
         id: id,
