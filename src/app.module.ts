4a43a56dfed884443cb8faedb8735017b3117924
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-13 16:03:13 +0900

    Feat: Add Chats module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 import { ImageModel } from './common/entity/image.entity';
 import { LogMiddleware } from './common/middleware/log.middleware';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
   imports: [
@@ -52,6 +53,7 @@ import { LogMiddleware } from './common/middleware/log.middleware';
     UsersModule,
     AuthModule,
     CommonModule,
+    ChatsModule,
   ],
   controllers: [AppController],
   providers: [
