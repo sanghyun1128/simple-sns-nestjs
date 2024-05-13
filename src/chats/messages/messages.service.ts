8ee76e36b7243309a73fd6516aacf6ba714c6fe7
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-13 20:56:03 +0900

    Feat: Add CreateMessagesDto for creating chat messages

---
+++ src/chats/messages/messages.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MessagesModel } from './entity/messages.entity';
+import { FindManyOptions, Repository } from 'typeorm';
+import { CommonService } from 'src/common/common.service';
+import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
+import { CreateMessagesDto } from './dto/create-messages.dto';
+
+@Injectable()
+export class ChatMessagesService {
+  constructor(
+    @InjectRepository(MessagesModel)
+    private readonly messagesRepository: Repository<MessagesModel>,
+    private readonly commonService: CommonService,
+  ) {}
+
+  async createMessage(dto: CreateMessagesDto) {
+    const message = await this.messagesRepository.save({
+      chat: { id: dto.chatId },
+      author: { id: dto.authorId },
+      message: dto.message,
+    });
+
+    return this.messagesRepository.findOne({
+      where: { id: message.id },
+    });
+  }
+
+  paginateChats(
+    dto: BasePaginationDto,
+    overrideFindOptions: FindManyOptions<MessagesModel>,
+  ) {
+    return this.commonService.paginate(
+      dto,
+      this.messagesRepository,
+      overrideFindOptions,
+      'messages',
+    );
+  }
+}
