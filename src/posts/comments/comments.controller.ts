00c84f2aa4f67280865b2cdd3d825deed7509b32
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-24 17:43:03 +0900

    feat: add getComments and getComment api

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { PaginateCommentDto } from './dto/paginate-comment.dto';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Get()
+  getComments(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Query() query: PaginateCommentDto,
+  ) {
+    return this.commentsService.paginateComments(postId, query);
+  }
+
+  @Get(':commentId')
+  getComment(@Param('commentId', ParseIntPipe) commentId: number) {
+    return this.commentsService.getCommentById(commentId);
+  }
 }
