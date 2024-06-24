b4be6748bfab5781d9dbd950a436a795d39b2809
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-24 18:18:27 +0900

    feat: Add postComment API endpoint to CommentsController

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { PaginateCommentDto } from './dto/paginate-comment.dto';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { CreateCommentDto } from './dto/create-comment-dto';
+import { User } from 'src/users/decorator/user.decorator';
+import { UsersModel } from 'src/users/entity/users.entity';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
@@ -17,5 +30,15 @@ export class CommentsController {
   @Get(':commentId')
   getComment(@Param('commentId', ParseIntPipe) commentId: number) {
     return this.commentsService.getCommentById(commentId);
+  }
+
+  @Post()
+  @UseGuards(AccessTokenGuard)
+  postComment(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() body: CreateCommentDto,
+    @User() user: UsersModel,
+  ) {
+    return this.commentsService.createComment(postId, body, user);
   }
 }
