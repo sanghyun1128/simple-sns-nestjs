beb0f389410c060192c8bbef4825c91bec5c7883
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-24 17:44:20 +0900

    feat: add comments paginate and get comment function

--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PaginateCommentDto } from './dto/paginate-comment.dto';
+import { CommonService } from 'src/common/common.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentsModel } from './entity/comments.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(CommentsModel)
+    private readonly commentsRepository: Repository<CommentsModel>,
+    private readonly commonService: CommonService,
+  ) {}
+
+  async paginateComments(postId: number, dto: PaginateCommentDto) {
+    return this.commonService.paginate(
+      dto,
+      this.commentsRepository,
+      {
+        where: {
+          post: {
+            id: postId,
+          },
+        },
+      },
+      `posts/${postId}/comments`,
+    );
+  }
+
+  async getCommentById(commentId: number) {
+    const comment = await this.commentsRepository.findOne({
+      where: {
+        id: commentId,
+      },
+    });
+
+    if (!comment) {
+      throw new BadRequestException('Comment not found');
+    }
+    return comment;
+  }
+}
