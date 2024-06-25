47956f024307228f23bf31b223189874f6b32f70
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-25 20:18:58 +0900

    feat: Implement patchComment API endpoint in CommentsController

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Query,
   UseGuards,
@@ -14,6 +15,7 @@ import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { CreateCommentDto } from './dto/create-comment-dto';
 import { User } from 'src/users/decorator/user.decorator';
 import { UsersModel } from 'src/users/entity/users.entity';
+import { UpdateCommentsDto } from './dto/update-comment.dto';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
@@ -40,5 +42,14 @@ export class CommentsController {
     @User() user: UsersModel,
   ) {
     return this.commentsService.createComment(postId, body, user);
+  }
+
+  @Patch(':commentId')
+  @UseGuards(AccessTokenGuard)
+  patchComment(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Body() body: UpdateCommentsDto,
+  ) {
+    return this.commentsService.updateComment(body, commentId);
   }
 }
