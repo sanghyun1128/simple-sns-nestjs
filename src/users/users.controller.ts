1f6274e9bf7cc359036a1fba00ad910515b683ca
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-26 14:22:26 +0900

    feat: Add follow functionality to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Roles } from './decorator/roles.decorator';
 import { RolesEnum } from './const/roles.const';
+import { UsersModel } from './entity/users.entity';
+import { User } from './decorator/user.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +13,17 @@ export class UsersController {
   @Roles(RolesEnum.ADMIN)
   getAllUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @Get('follow/me')
+  async getFollow(@User() user: UsersModel) {
+    return this.usersService.getFollowers(user.id);
+  }
+
+  @Post('follow/:id')
+  async postFollow(@User() user: UsersModel, @Param('id') followeeId: number) {
+    await this.usersService.followUser(user.id, followeeId);
+
+    return true;
   }
 }
