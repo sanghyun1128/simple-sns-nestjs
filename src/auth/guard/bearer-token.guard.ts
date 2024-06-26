f7210158dff51762513cc04dd99fdbf3e5032fa7
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-26 10:36:09 +0900

    feat: Update BearerTokenGuard to use IsPublic decorator

--- src/auth/guard/bearer-token.guard.ts
+++ src/auth/guard/bearer-token.guard.ts
@@ -6,14 +6,26 @@ import {
 } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { UsersService } from 'src/users/users.service';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from 'src/common/decorator/is-public.decorator';
 
 @Injectable()
 export class BearerTokenGuard implements CanActivate {
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
+    private readonly reflector: Reflector,
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const req = context.switchToHttp().getRequest();
 
     const rawToken = req.headers['authorization'];
