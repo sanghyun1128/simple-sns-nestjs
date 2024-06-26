be88e83cb98e8d08e1cab90e1e37ca4acab05866
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-26 11:33:12 +0900

    feat: Add role-based authorization to getAllUsers endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Roles } from './decorator/roles.decorator';
+import { RolesEnum } from './const/roles.const';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Roles(RolesEnum.ADMIN)
   getAllUsers() {
     return this.usersService.getAllUsers();
   }
