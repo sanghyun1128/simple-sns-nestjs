6f9ee7a2e7ecf45488e14616e7ceccaf3ad818bd
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-26 09:14:03 +0900

    feat: Add PostExistsMiddleware to CommentsModule

--- src/posts/comments/comments.module.ts
+++ src/posts/comments/comments.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +6,8 @@ import { CommentsModel } from './entity/comments.entity';
 import { CommonModule } from 'src/common/common.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
+import { PostExistsMiddleware } from './middleware/post-exists.middleware';
+import { PostsModule } from '../posts.module';
 
 @Module({
   imports: [
@@ -13,8 +15,13 @@ import { UsersModule } from 'src/users/users.module';
     CommonModule,
     AuthModule,
     UsersModule,
+    PostsModule,
   ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
-export class CommentsModule {}
+export class CommentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PostExistsMiddleware).forRoutes(CommentsController);
+  }
+}
